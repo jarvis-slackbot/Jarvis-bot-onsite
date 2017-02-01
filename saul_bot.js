37d6fb26d610eddb43266ded8ab73d704a5764a6
Saul Lopez <slopez15@asu.edu>
2017-02-01 01:56:55 -0700

    starting bot fiddling for auth.

---
+++ saul_bot.js
@@ -0,0 +1,67 @@
+/*Docs
+Node
+	querystring
+	http
+AWS
+	Lambda
+	API Gateway
+Claudia
+	
+BotKit
+	
+Slack
+	
+*/
+//Library
+	const aws = require('aws-sdk');
+	//Specific Library Area
+		//Node
+	const qs = require('querystring');
+	const http = require('http');
+		//AWS
+	const lambda = new aws.Lambda();
+
+		//Claudia
+	const botBuilder = require('claudia-bot-builder');
+		//BotKit
+	//empty
+		//Slack
+	//empty
+
+//=============================== START ===============================
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//=============================== END ===============================
+/*
+//Authorization Keys
+    // Add new AWS commands here
+    AWSCommands:{
+        status: "status"
+    }
+};
+*/
