fd04f26425ea1f08702215e2ed90a349195ea059
Saul Lopez <slopez15@asu.edu>
2017-02-07 19:20:20 -0700

    save work so far. Looking for Notes in git's dangling blobs

--- practice-saul/api-builder.js
+++ practice-saul/api-builder.js
@@ -8,46 +8,13 @@ module.exports = api;
 api.get('/hello', function () {
 	return "Hello There.";
 });
-api.get('/', function (request) {
+api.get('/super', function (request) {
 	return request.queryString.name + ' is ' + superb();
 });
+//console.log('THIS: ' + JSON.stringify(api));
 
 
 /* 
 Lookup claudia-api-builder ojbect constructor.
 Not sure how exporting objects work... propably a design pattern and not a function module type of export
 */
-
-
-/* FROM Example
-var ApiBuilder = require('claudia-api-builder')
-	, api = new ApiBuilder()
-	, superb = require('superb')
-//    , express = require('express')
-//    , app = express()
-    ;
-module.exports = api;
-
-api.get('/greet', function (request) {
-	return request.queryString.name + ' is ' + superb();
-});
-
-console.log('THIS: ' + JSON.stringify(api));
-//api.forEach(function(obj){
-//    console.log(obj);
-//});
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
