c908856cda5683cb4f140b659a55cc3fe5631398
Saul Lopez <slopez15@asu.edu>
2017-02-08 00:40:38 -0700

    update all files and test pull vs merge

--- practice-saul/bot-builder.js
+++ practice-saul/bot-builder.js
@@ -2,8 +2,11 @@ var botBuilder = require('claudia-bot-builder'),
     excuse = require('huh');
 
 module.exports = botBuilder(function (request) {
-  return 'Thanks for sending ' + request.text  + 
-      '. Your message is very important to us, but ' + 
-      excuse.get();
+    console.log("console log test.");
+    console.log("full request, " + JSON.stringify(request.originalRequest))
+    return ' Thanks for sending ' + request.text +
+        '. Your message is very important to us, but ' +
+        excuse.get();
 });
-
+//Couldn't see if this worked well, needed an access token or configure with something(from possible configurations) to obtain one.
+//In video, person sent a message/request and replied back with same info, like above.
