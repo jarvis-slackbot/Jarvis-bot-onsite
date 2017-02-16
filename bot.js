55e090d94eb366e320fb8ee2524a72b771b472ca
Saul Lopez <slopez15@asu.edu>
2017-02-15 19:47:10 -0700

    placed setEnvironments require in bot.

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 'use strict';
-require('./auth.js');
+require('./setEnvironments.js');
 const aws = require('aws-sdk');
 const promiseDelay = require('promise-delay');
 var botBuilder = require('claudia-bot-builder');
