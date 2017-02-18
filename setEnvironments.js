ec6e3fa5deb3efc5d27ce84c3ef77f7f4beef1c5
Saul Lopez <slopez15@asu.edu>
2017-02-17 17:49:53 -0700

    Fixed eror. Reading, writing lambda

--- setEnvironments.js
+++ setEnvironments.js
@@ -1,10 +1,7 @@
 //Developed to set environment variables at runtime for security reasons
-var fs = require('fs');
 //var AWS = require('aws-sdk');
-var pathToFile = "./.env"; //env.json
-var bufferObj = fs.readFileSync(pathToFile);
-var string = bufferObj.toString();
-var json = JSON.parse(string);
+var json = require('./.env.js').json;
+
 
 //console.log(json.SLACK);
 //console.log(json.AWS);
@@ -12,12 +9,12 @@ var json = JSON.parse(string);
 /**/
 //SETTING ENVIRONMENT VARIABLES
     //SLACK
-    process.env.AWS_PROFILE = json.SLACK.AWS_PROFILE;
     process.env.SLACK_CLIENT_ID = json.SLACK.SLACK_CLIENT_ID;
     process.env.SLACK_CLIENT_SECRET = json.SLACK.SLACK_CLIENT_SECRET;
     process.env.SLACK_VERIFICATION_TOKEN = json.SLACK.SLACK_VERIFICATION_TOKEN;
     process.env.SLACK_TEST_TOKEN = json.SLACK.SLACK_TEST_TOKEN;
     //AWS
+    process.env.AWS_PROFILE = json.AWS.AWS_PROFILE;
     process.env.AWS_ACCESS_KEY_ID = json.AWS.AWS_ACCESS_KEY_ID;
     process.env.AWS_SECRET_ACCESS_KEY = json.AWS.AWS_SECRET_ACCESS_KEY;
     process.env.AWS_REGION = json.AWS.AWS_REGION;
