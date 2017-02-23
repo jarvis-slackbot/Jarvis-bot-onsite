1a01cb46807cff6163ef5eb1bc283c5fbf7c31cc
Saul Lopez <slopez15@asu.edu>
2017-02-23 07:22:59 -0700

    Printing S3 bucket names, left some comments including possible method exports and a possible headBucket section. cleaned up a bit.

--- s3.js
+++ s3.js
@@ -23,97 +23,98 @@ module.exports = {
     //list avaiable buckets
     getBuckets : function (){
         
-        
-        return new Promise(function (resolve, reject) {
-            
+        return new Promise(function (resolve, reject) {    
+
             var slackMsg = new SlackTemplate();
             
-            /*
-            --DELETE-- Temp list. possible methods.
-            (AWS.Request) getBucketAcl(params = {}, callback)
-                Gets the access control policy for the bucket.
-            (AWS.Request) getBucketLocation(params = {}, callback)
-                Returns the region the bucket resides in.
-            (AWS.Request) headBucket(params = {}, callback)
-                This operation is useful to determine if a bucket exists and you have permission to access it.
-            (AWS.Request) headObject(params = {}, callback)
-                The HEAD operation retrieves metadata from an object without returning the object itself.
-            (AWS.Request) listBuckets(params = {}, callback)
-                Returns a list of all buckets owned by the authenticated sender of the request.
-            */
-            
-//            var results; //holder; to return via resolve
-            
-            if (1){
-                /*
-                var params_headBucket = {
-                    Bucket: 'STRING_VALUE' //REQUIRED
-                };
-                */
-                var info = []; //collect data.Buckets .Name?
-                s3Data.listBuckets({}, function callback (err, data){
-                    if(err){
-                        console.log(err, err.stack);
-                        reject("rejected!"/*msg.errorMessage(err.message)*/);
-                    }
-                    else {//code
-                        var buckets = data.Buckets //.Buckets, array<map>, name-creationDate; .Owner, map, DisplayName-ID
-                        
-                        buckets.forEach(function (bucket) {
-                            var name = bucket.Name;
-                            info.push(name);
-                        });
-                        
-                        resolve(info);
-                    }
-                });
-                /*
-                var bucketHead = s3Data.headBucket(params_headBucket, function callback (err, data){
-                        if(err){
-                            console.log(err, err.stack);
-                            reject(msg.errorMessage(err.message));
-                        }
-                        //code
-                    //info.push(data);
-                });
-                */
-                
-                //+ "info0:: " + info[0]
-                //+ "info1:: " + info[1]
+            var info = []; //collect data.Buckets.Name
+            s3Data.listBuckets({}, function callback (err, data){
+                if(err){
+                    //console.log(err, err.stack);
+                    reject(msg.errorMessage(err.message));
+                }
+                else {//code
+                    //.Buckets returns array<map> with name & creationDate; .Owner returns map with DisplayName & ID
+                    var buckets = data.Buckets;
+                    buckets.forEach(function (bucket) {
+                        var name = bucket.Name;
+                        info.push(name);
+                    });
 
+
+                    //slack message formatting
+                    slackMsg.addAttachment(msg.getAttachNum());
+                    var text = '';
+
+                    if (info.length > 0){
+                        info.forEach(function(name){
+                            text += "S3 bucket: " + name + "\n";
+                        });
+                        slackMsg.addText(text);
+                        resolve(slackMsg);
+                    }
+                    else {
+                        text = "There are no S3 buckets.";
+                        slackMsg.addText(text);
+                        resolve(slackMsg);
+                    }
+                }
+            });
+
+
+
+
+            /*
+            //use bucket names from other function then use that to for each below code by changing params_headBucket.Bucket and calling method
+            var params_headBucket = {
+                Bucket: 'STRING_VALUE' //REQUIRED
+            };
+            var allHeadBuckets = [];
+            s3Data.headBucket(params_headBucket, function callback (err, data){
+                if(err){
+                    //console.log(err, err.stack);
+                    reject(msg.errorMessage(err.message));
+                }
+                else {//code
                 
-                
-                
-                /*
-                //slack message formatting
-                slackMsg.addAttachment(msg.getAttachNum());
-                var text = '';
-                text = "\n\nlist all buckets owned by the authenticated sender of the request:: "
-                    + " \nJSON.stringify:: "
-                    + JSON.stringify(bucketList)
-                    + " \ntoString():: "
-                    + bucketList.toString();
-                    + " \ndry:: "
-                    + bucketList
-                    + " \ncheck if bucket exists and you have permission to access it:: " 
-                    //+ bucketHead
-                ;
-                slackMsg.addText(text);
-                resolve(slackMsg);*/
-            }
-            else{
-                reject("rejected!"/*msg.errorMessage("something went wrong! in s3.js")*/);
-            }
+//                    var extracts = data.SOMETHING;
+                    extracts.forEach(function (extract) {
+                        var something = extract.something;
+                        allHeadBuckets.push(something);
+                    });
+
+
+                    //slack message formatting
+                    slackMsg.addAttachment(msg.getAttachNum());
+                    var text = '';
+
+                    if (allHeadBuckets.length > 0){
+                        allHeadBuckets.forEach(function(extract){
+                            text += "headbucket: " + something + "\n";
+                        });
+                        slackMsg.addText(text);
+                        resolve(slackMsg);
+                    }
+                    else {
+                        text = "There are no headbuckets.";
+                        slackMsg.addText(text);
+                        resolve(slackMsg);
+                    }
+                }
+            });
+            */
+
             
-            
-            /*method().then(()=>{
-                resolve({});
-            }).catch((err)=>{
+        }).catch((err)=>{
                 reject(msg.errorMessage(err));
-            });*/
-            
         });
     }/*,
+    
+    
+    
+    
+    
+    
     getBucketNames : function (){
         
         return new Promise(function (resolve, reject){
@@ -134,7 +135,23 @@ module.exports = {
 
 
 
+/* SCRATCH CODE
 
+--DELETE-- Temp list. possible methods.
+(AWS.Request) getBucketAcl(params = {}, callback)
+    Gets the access control policy for the bucket.
+(AWS.Request) getBucketLocation(params = {}, callback)
+    Returns the region the bucket resides in.
+(AWS.Request) headBucket(params = {}, callback)
+    This operation is useful to determine if a bucket exists and you have permission to access it.
+(AWS.Request) headObject(params = {}, callback)
+    The HEAD operation retrieves metadata from an object without returning the object itself.
+(AWS.Request) listBuckets(params = {}, callback)
+    Returns a list of all buckets owned by the authenticated sender of the request.
+
+
+
+*/
 
 
 
